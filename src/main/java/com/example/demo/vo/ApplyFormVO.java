8ec7160ea11953044edfdd93fa1b3e61c2b6b581
김도영 <rlaehdud4992@ssu.ac.kr>
2019-11-11 14:27:36 +0900

    1. ApplyForm 모델의 Repository와 VO 생성 2. apply.jsp에서 작성한 폼을 Interface에 있는 변수에 입력되게 했음

---
+++ src/main/java/com/example/demo/vo/ApplyFormVO.java
@@ -0,0 +1,18 @@
+package com.example.demo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApplyFormVO {
+    private String name;
+
+    private String phone;
+
+    private String introduceMyself;
+
+    private String motive;
+
+    private Boolean isAgreePersonalInformationCollection;
+}
